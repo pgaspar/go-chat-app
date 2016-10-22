a96ac3aac224924c6f97859b65f9a2977e03028d
Pedro Gaspar <pedro.gaxpar@gmail.com>
2016-10-22 15:49:51 +0100

    Template variables!

--- hello.go
+++ hello.go
@@ -1,9 +1,14 @@
+// Go is procedural. There are no classes, only structs
+
 // Install fresh (go get github.com/pilu/fresh), cd to work dir & run `fresh`
 
 // := does type inference
 // = does not. a := 1; a = "abc" will break.
 // var i float32 declares i as a float
 // var i = 1 also infers
+
+// This is weird. If you include a private var in a template it stops execution and gives you half a page, basically - simply stopping execution.
+// However, no proper error is presented...
 
 package main
 
@@ -13,7 +18,20 @@ import "html/template"
 var templates = template.Must(template.ParseGlob("templates/*.tmpl"))
 
 func handler(w http.ResponseWriter, r *http.Request) {
-  templates.ExecuteTemplate(w, "index", nil)
+  type Index struct {
+    // If it starts with capital letter it's public.
+    // If it doesn't, it's private, so you can't use it in the templates.
+    Name string // default: ""
+    name string // default: ""
+  }
+
+  // data := Index { "pedro", "yooooo" }
+  data := Index {
+    Name: "pedro",
+    name: "yooooo",
+  }
+
+  templates.ExecuteTemplate(w, "index", &data)
 }
 
 func main() {
